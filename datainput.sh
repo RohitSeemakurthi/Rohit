90557d9934a6c20cff746e084083a56c3119338b
rohit <rohitssr67@gmail.com>
2024-08-30 08:17:57 -0400

    trying new with data inputs

---
+++ datainput.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#!/bin/bash
+# Ask the user for their name
+echo “What’s your name?”
+read name
+# Greet the user
+echo “Hello, $name! Nice to meet you.”
+
+
+#!/bin/bash
+
+echo "what is your phone number"
+read phonenumber
+echo "sir ur phone number is $phonenumber, thank you nice to meet u"
