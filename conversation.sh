249da9f14d2f0a0030a2556bc1740b42ae331228
rohit <rohitssr67@gmail.com>
2024-08-30 08:11:13 -0400

    new changes for conversation

--- conversation.sh
+++ conversation.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "rohit: hi rahul how are u"
-echo "rahul: hello rohit i am fine"
-echo "rohit: what are u dng"
-echo "rahul: i am doing nothing"
+name1="ram"
+name2="raheem"
+
+echo "$name1: hi $name2 how are u"
+echo "$$name2: hello $name1 i am fine"
+echo "$name1: what are u dng"
+echo "$name2: i am doing nothing"
