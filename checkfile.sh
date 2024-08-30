fd351e5eb21550f87b6fa49c4a5f343219576115
rohit <rohitssr67@gmail.com>
2024-08-30 09:11:25 -0400

    msg

--- checkfile.sh
+++ checkfile.sh
@@ -1,10 +1,7 @@
 
 
 #!/bin/bash
-file=”helloworld.sh”
-# Check if the file exists
-if [ -e “$file” ]; then
-echo “File exists: $file”
-else
-echo “File not found: $file”
-fi
+
+abcd=”Hello”
+echo ‘$abcd’ # Output: $abcd
+echo “$abcd” # Output: Hello
